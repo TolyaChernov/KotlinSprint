3dde33d7d1155cf5215cd111ac806d313c93ab3a
Anatol <chernov.py.dev@gmail.com>
2024-11-22 21:36:01 +0200

    ks-1-1 add task 1

---
+++ src/main/kotlin/Lesson_1.kt
@@ -0,0 +1,12 @@
+package org.example
+
+fun main() {
+    task_1()
+}
+
+fun task_1() {
+    val countOrders: Int = 75
+    val thankShopping: String = "Спасибо за покупку!!!"
+    println(countOrders)
+    println(thankShopping)
+}
