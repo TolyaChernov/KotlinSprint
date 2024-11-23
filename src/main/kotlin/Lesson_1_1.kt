df4cf64808264abdb38487fa604407a5481681fa
Anatol <chernov.py.dev@gmail.com>
2024-11-23 18:52:38 +0200

    KS-1-1 fix task 1

---
+++ src/main/kotlin/Lesson_1_1.kt
@@ -0,0 +1,17 @@
+package org.example
+
+fun main() {
+//    task_1()
+    val countOrders: Int = 75
+    val thankShopping: String = "Спасибо за покупку!!!"
+    println(countOrders)
+    println(thankShopping)
+}
+
+
+//fun task_1() {
+//    val countOrders: Int = 75
+//    val thankShopping: String = "Спасибо за покупку!!!"
+//    println(countOrders)
+//    println(thankShopping)
+//}
