305c5805c677d1731fddcba2565e50ea5fcfa0d2
Anatol <chernov.py.dev@gmail.com>
2024-11-23 08:36:45 +0200

    KS-1-4

---
+++ src/main/kotlin/lesson_1_4.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val length: Long = 40_868_600_000 // миллиметры
+    val age: Byte = 27 // лет
+    val partOfDay: Float = 0.075f // доля дня
+    val seconds: Int = 6480 // секунд
+    val partOfYear: Double = 2.0547945205479453E-4 // доля года
+    val apogee: Int = 327000 // метры
+
+    println("Расстояние: $length мм")
+    println("Возраст: $age лет")
+    println("Часть дня: $partOfDay")
+    println("Секунды: $seconds сек")
+    println("Часть года: $partOfYear")
+    println("Апогей: $apogee м")
+}
